6f0b4371c99c5eb3d714575725d42c5a13043fcb
Ϳαγ ᾥίῘῘίαm Sαρατ <jaywilliamsapar@gmail.com>
2022-04-04 19:16:20 -0800

    Initial Commit

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-var mySong = document.getElementsById("mySong");
+var mySong = document.getElementById("mySong");
 var icon = document.getElementById("icon");
 
-icon.onclick = function() {
-    mySong.play();
+
+icon.onclick = function () {
+    if (mySong.paused) {
+        mySong.play();
+        icon.src = "/DL_Images/pause.png"
+    } else {
+        mySong.pause();
+        icon.src = "/DL_Images/play.png"
+    }
 }
